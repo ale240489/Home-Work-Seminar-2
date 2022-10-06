c1579ab499fbaaedb53c4d1f5fe8f7162394b35d
Aleksandr <ale240489@yandex.ru>
2022-10-06 18:02:29 +0300

    Work3

---
+++ Work3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли это день выходным.
+
+Console.WriteLine("Введите цифру от одного до 7");
+string num11 = Console.ReadLine();
+int num1 = int.Parse(num11);
+
+void dayOfWeak (int a)
+{
+    int numberDay = a;
+    if (a >= 1 && a<=5)
+        Console.WriteLine("рабочий");
+    else if (a > 5 && a < 8)
+        Console.WriteLine("выходной");
+    else
+        Console.WriteLine("Ведено неверное число");
+    
+}
+dayOfWeak(num1);
