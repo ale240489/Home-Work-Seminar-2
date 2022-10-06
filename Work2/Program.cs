ad4bb13b130ab7a565e0c1394add3ed114ab1293
Aleksandr <ale240489@yandex.ru>
2022-10-06 18:01:52 +0300

    Work 2

---
+++ Work2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа,
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное число");
+string num11 = Console.ReadLine();
+int num1 = int.Parse(num11);
+
+void thirdNum(int a)
+{
+    string num = a.ToString();
+    int third = int.Parse(num[2].ToString());
+    Console.WriteLine(third);
+}
+
+if (num1 > 99)
+    thirdNum(num1);
+else
+    Console.WriteLine($"В числе {num1} нет третьей цифры");
