545a4f566cb28bd2e7a20a787d6e5126cb38be86
Aleksandr <ale240489@yandex.ru>
2022-10-06 18:01:19 +0300

    Work1

---
+++ Work1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число на выходе
+// показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+string num11 = Console.ReadLine();
+int num1 = int.Parse(num11);
+
+void secondNum(int a)
+{
+    string num = a.ToString();
+    int second = int.Parse(num[1].ToString());
+
+    if (a > 99)
+        Console.WriteLine(second);
+    else
+        Console.WriteLine("Вы ввели не трехзначное число");
+}
+
+secondNum(num1);
